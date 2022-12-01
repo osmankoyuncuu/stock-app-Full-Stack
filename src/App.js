3f1bca9e103b22e8e9a103f27254eeaf1c94cf91
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-12-01 17:18:33 +0300

    add tremor

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import store, { persistor } from "./app/store";
 import { ToastContainer } from "react-toastify";
 import { PersistGate } from "redux-persist/integration/react";
+import "@tremor/react/dist/esm/tremor.css";
 
 function App() {
   const theme = createTheme({
