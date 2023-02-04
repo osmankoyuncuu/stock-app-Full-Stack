ac091498f96585e84e5508ab5db5c871fbd27b09
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2023-02-04 22:42:59 +0300

    add brand, firm, product, purchases

--- backend/stock/serializers.py
+++ backend/stock/serializers.py
@@ -19,9 +19,20 @@ class CategorySerializer(serializers.ModelSerializer):
         return Product.objects.filter(category_id=obj.id).count()
     
 class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+    brand = serializers.StringRelatedField()
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = (
+            "id",
+            "name",
+            "category",
+            "category_id",
+            "brand",
+            "brand_id",
+            "stock",    
+        )
+        read_only_fields = ("stock",)
     
 class CategoryProductSerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True)
@@ -33,3 +44,70 @@ class CategoryProductSerializer(serializers.ModelSerializer):
         
     def get_product_count(self, obj):
         return Product.objects.filter(category_id=obj.id).count()
+    
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = (
+            'id',
+            'name',
+            'image'
+        )
+        
+class FirmSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Firm
+        fields = (
+            'id',
+            'name',
+            'phone',
+            'image',
+            'address'
+        )
+
+class PurchasesSerializer(serializers.ModelSerializer):
+    
+    user = serializers.StringRelatedField() 
+    firm = serializers.StringRelatedField()
+    brand = serializers.StringRelatedField()
+    product = serializers.StringRelatedField()
+    product_id = serializers.IntegerField()
+    brand_id = serializers.IntegerField()
+    firm_id = serializers.IntegerField()
+    category = serializers.SerializerMethodField()
+    time_hour = serializers.SerializerMethodField()
+    createds = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Purchases
+        fields = (
+            "id",
+            "user",
+            "user_id",
+            "category",
+            "firm",
+            "firm_id",
+            "brand",
+            "brand_id",
+            "product",
+            "product_id",
+            "quantity",
+            "price",
+            "price_total",
+            "time_hour",
+            "createds",
+        )
+        
+    
+    # def get_category(self, obj):
+    #     product = Product.objects.get(id=obj.product_id)
+    #     return Category.objects.get(id=product.category_id).name
+    
+    #def get_category(self, obj):
+    #    return obj.product.category.name
+    
+    #def get_time_hour(self, obj):
+    #    return datetime.datetime.strftime(obj.createds, "%H:%M")
+    
+    #def get_createds(self, obj):
+    #    return datetime.datetime.strftime(obj.createds, "%d,%m,%Y")
--- backend/stock/views.py
+++ backend/stock/views.py
@@ -7,7 +7,7 @@ from .models import (
     Purchases,
     Sales
 )
-from .serializers import CategorySerializer, CategoryProductSerializer
+from .serializers import CategorySerializer, CategoryProductSerializer,BrandSerializer,FirmSerializer,ProductSerializer, PurchasesSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import DjangoModelPermissions
 
@@ -23,3 +23,33 @@ class CategoryView(viewsets.ModelViewSet):
         if self.request.query_params.get("name"):
             return CategoryProductSerializer        
         return super().get_serializer_class()
+    
+class BrandView(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+    permission_classes = [DjangoModelPermissions]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+    
+class FirmView(viewsets.ModelViewSet):
+    queryset = Firm.objects.all()
+    serializer_class = FirmSerializer
+    permission_classes = [DjangoModelPermissions]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+    
+class ProductView(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [DjangoModelPermissions]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['category', 'brand']
+    search_fields = ['name']
+    
+class PurchaseView(viewsets.ModelViewSet):
+    queryset = Purchases.objects.all()
+    serializer_class = PurchasesSerializer
+    permission_classes = [DjangoModelPermissions]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['firm', 'product']
+    search_fields = ['firm']   
